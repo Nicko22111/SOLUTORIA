0d1a725c852f856bf40745d29c57feaed69f859b
Nicko22111 <nicoms2012@gmail.com>
2022-07-01 19:06:41 -0400

    Cambios Vistas, Controlador y Rutas

---
+++ appstarter/app/Controllers/Uf.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Controllers;
+
+class Uf extends BaseController
+{
+    public function index()
+    {
+        return view('uf/historico');
+    }
+}
---
+++ appstarter/app/Views/uf/historico.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Historico de UF</title>
+    <meta name="description" content="The small framework with powerful features">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
+
+    <!-- Bootstrap CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+    <!-- echarts js -->
+    <script type="text/javascript" src="<?= base_url() ?>/echarts/dist/echarts.js"></script>
+</head>
+<body>
+
+<!-- HEADER: MENU + HEROE SECTION -->
+<header>
+
+</header>
+
+<!-- CONTENT -->
+<div class="container">
+    <section>
+
+        <h1>Historico de UF</h1>
+
+        <p>Esta es una prueba en la vista:</p>
+
+
+    </section>
+</div>
+
+
+<!-- SCRIPTS -->
+<script type="text/javascript">
+
+</script>
+
+<!-- -->
+
+</body>
+</html>
